5b415ec12690b828a67cac9e7f8b14960fb80cd9
Anna C <anna.s.chong@gmail.com>
2020-10-04 21:53:55 +1100

    ADD project data

---
+++ src/components/Projects.js
@@ -0,0 +1,44 @@
+export const Projects = [
+  {
+    image: "assets/tinySteps.png",
+    title: "Tiny Steps",
+    url: "https://tinystepsau.herokuapp.com/",
+    github: "https://github.com/Nikki-Rees/Project_2",
+    description: "Want to develop a healthy habit? This app will help you develop healthy habits and maintain motivation by tracking progress."
+  },
+  {
+    image: "assets/burger.png",
+    title: "Burger Record",
+    url: "https://shrouded-bayou-18775.herokuapp.com/",
+    github: "https://github.com/acho9138/Burger-Record",
+    description: "Losing track of all the burgers you want to eat? Use this app to keep track of them all and check them off as you devour it."
+  },
+  {
+    image: "assets/jobSeeker.png",
+    title: "Job Seeker",
+    url: "https://guedesantonio.github.io/JOB-SEARCH-APP/",
+    github: "https://github.com/guedesantonio/JOB-SEARCH-APP",
+    description: "Looking for a career change? You've come to the right place! Search jobs which interest you and get book recommendations which can help boost your chances."
+  },
+  {
+    image: "assets/weather.png",
+    title: "Weather App",
+    url: "https://acho9138.github.io/Weather-App/",
+    github: "https://github.com/acho9138/Weather-App",
+    description: "Ever wonder what the weather is like in a particular city? Search cities using this app and it'll show you the current weather conditions as well as the forcast for the next 5 days."
+  },
+  {
+    image: "assets/planner.png",
+    title: "Work Day Planner",
+    url: "https://acho9138.github.io/HW5-Work-Day-Planner/",
+    github: "https://github.com/acho9138/HW5-Work-Day-Planner",
+    description: "Need to keep track of your day? Plan each hour of the day the past, current and future hours colour-coded."
+  },
+  {
+    image: "assets/password.png",
+    title: "Password Generator",
+    url: "https://acho9138.github.io/HW3-Password-Generator/",
+    github: "https://github.com/acho9138/HW3-Password-Generator",
+    description: "Need a strong password? This app generates a strong password for you with all the requirements you are looking for."
+  }
+]
