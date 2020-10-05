33adeee1a51ea75b3cbde1ad915ef544b03becff
Anna C <anna.s.chong@gmail.com>
2020-10-05 17:53:12 +1100

    Updates

---
+++ precache-manifest.a554b3b1941ac4eeeaa7d906736ae319.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2055dd332b3e02889ea45c9f35f7e8ab",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "6e6ede421b77fa1cee02",
+    "url": "/React-Portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "18c45f6706b664052e91",
+    "url": "/React-Portfolio/static/js/2.b6a4fe63.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React-Portfolio/static/js/2.b6a4fe63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e6ede421b77fa1cee02",
+    "url": "/React-Portfolio/static/js/main.02714ce9.chunk.js"
+  },
+  {
+    "revision": "bcebe2d38e8b197ca4b9",
+    "url": "/React-Portfolio/static/js/runtime-main.266d4783.js"
+  }
+]);
