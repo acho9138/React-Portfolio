2dce1552af0aa674495ce2c6461bdcd2cbd294bd
Anna C <anna.s.chong@gmail.com>
2020-10-05 17:40:47 +1100

    ADD footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,50 @@
+// React Libraries
+import React from 'react';
+
+// Material UI
+import { Grid, Container, Typography } from '@material-ui/core';
+
+// Custom Styles
+import { styles } from './Footer.styles';
+
+
+export const Footer = () => {
+  const classes = styles();
+
+  return (
+    <div className={classes.root}>
+      <Container maxWidth="sm">
+        <Grid container spacing={3}>
+          <Grid item xs={12} align="center">
+            <Typography variant="h5" gutterBottom>
+              Contact Information
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={4} align="center">
+            <Typography variant="body1" gutterBottom>
+              <i className="fas fa-map-marker-alt fa-lg"></i>&nbsp;&nbsp;Sydney, Australia
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={3} align="center">
+            <Typography variant="body2" gutterBottom>
+              <i className="fas fa-mobile-alt fa-lg"></i>&nbsp;&nbsp;0433 586 311
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={5} align="center">
+            <Typography variant="body2" gutterBottom>
+              <i className="far fa-envelope fa-lg"></i>&nbsp;&nbsp;anna.s.chong@gmail.com
+            </Typography>
+          </Grid>
+          <Grid item xs={12} align="center">
+            <Typography variant="body1" gutterBottom>
+              <a href="https://github.com/acho9138" target="_blank" rel="noopener" className={classes.icons}><i className="fab icons fa-github fa-3x"></i></a>
+              <a href="https://www.linkedin.com/in/anna-chong-199167191/" target="_blank" rel="noopener" className={classes.icons}><i
+                className="fab icons fa-linkedin fa-3x"></i></a>
+              <a href="./assets/resume.pdf" target="_blank" className={classes.icons}><i className="fas icons fa-portrait fa-3x"></i></a>
+            </Typography>
+          </Grid>
+        </Grid>
+      </Container>
+    </div >
+  );
+}
---
+++ src/components/Footer/Footer.styles.js
@@ -0,0 +1,18 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const styles = makeStyles(() => ({
+  root: {
+    width: "100%",
+    backgroundColor: '#0b3954',
+    marginTop: "20px",
+    paddingTop: "50px",
+    bottom: 0,
+    position: "relative",
+    color: "white",
+  },
+  icons: {
+    color: "white",
+    paddingLeft: "15px",
+    paddingRight: "15px"
+  }
+}));
