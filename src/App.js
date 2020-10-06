b5eaedf027fad7355fa54bf5ea96d23d80dae288
Anna C <anna.s.chong@gmail.com>
2020-10-06 15:51:39 +1100

    UPDATE routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 import './App.css';
 import { Navbar, Footer } from './components';
 import { About } from "./pages/About/About";
@@ -11,12 +11,12 @@ function App() {
     <>
       <Navbar />
       <Router basename={process.env.PUBLIC_URL}>
-        <Route exact path="/">
-          <Redirect to="/about" />
-        </Route>
-        <Route exact path="/about" component={About} />
-        <Route exact path="/portfolio" component={Portfolio} />
-        <Route path='*' exact={true} component={NotFound} />
+        <Switch>
+          <Route exact path="/" component={About} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route path="*" exact={true} component={NotFound} />
+        </Switch>
       </Router>
       <Footer />
     </>
@@ -24,3 +24,6 @@ function App() {
 }
 
 export default App;
+
+/* <Redirect to="/about" />
+          </Route> */
